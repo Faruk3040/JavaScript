b2e89aefe16921f292b3ff32ead1e068e7cd4ead
Faruk3040 <farukahmed3040@gmail.com>
2021-06-14 15:25:00 +0600

    javascript

---
+++ script.js
@@ -0,0 +1,44 @@
+//let js = 'amazing';
+//if (js === 'amazing') alert('JavaScript is Fun!');
+//console.log(40 + 30 + 10 - 20);
+
+let js = 'amazing';
+if (js === 'amazing') alert('javascript is fun!');
+40 + 30 + 10 - 20;
+console.log(40 + 30 + 10 - 20);
+console.log("Faruk Ahmed");
+
+let firstName = "Faruk";
+let lastName = "Ahmed";
+console.log(firstName);
+console.log(lastName);
+
+let person = 'faruk';
+let PI = 3.1415;
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'web developer';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let country = 'Bangladesh';
+let continent = 'Asia';
+let population = 14;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'Bangla';
+//const country = 'Bangladesh';
+//const continent = 'Asia';
+//const isIsland = false;
+//isIsland = true;
